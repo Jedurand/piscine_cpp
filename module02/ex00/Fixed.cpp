3720526d6861ae3306ccbcdfa4d5f57421addc6f
Hieronimus <jerome.d274@gmail.com>
2020-03-19 15:00:03 +0100

    Fixed point

---
+++ module02/ex00/Fixed.cpp
@@ -0,0 +1,41 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+	_n = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+Fixed::Fixed(const Fixed& ori)
+{
+	std::cout << "Copy constructor called\n";
+	_n = ori.getRawBits();
+}
+
+void Fixed::operator = (const Fixed& ori)
+{
+	std::cout << "Assignation member function called\n";
+	this->_n = ori.getRawBits();
+//	Fixed fixed(ori);
+//	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return (_n);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called\n";
+	_n = raw;
+}
+
+
+
---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <bitset>
+
+class Fixed
+{
+	private:
+	int 			_n;
+	static const int	nbits = 8;
+
+	public:
+	Fixed();
+	void operator = (const Fixed& ori);
+	Fixed(const Fixed& ori);
+	~Fixed();
+
+	int getRawBits(void) const; 
+	void setRawBits(int const raw);
+};
+
+#endif
