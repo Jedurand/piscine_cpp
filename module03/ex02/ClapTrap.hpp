7f26e023c5eb1524c27f3bd8b9c3d1f237933e9a
Hieronimus <jerome.d274@gmail.com>
2020-03-20 16:20:27 +0100

    ClapTrap

---
+++ module03/ex02/ClapTrap.hpp
@@ -0,0 +1,35 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+
+class ClapTrap
+{
+	public:
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& ori);
+	~ClapTrap();
+	
+	void operator = (const ClapTrap& ori);
+
+	void rangedAttack(const std::string& target);
+	void meleeAttack(const std::string& target);
+	void takeDamage(unsigned int ammount);
+	void beRepaired(unsigned int ammount);
+
+	protected:
+	std::string	_name;
+	unsigned int 	_hitPoints;
+	unsigned int 	_maxHitPoints;
+	unsigned int 	_energyPoints;
+	unsigned int	_maxEnergyPoints;
+	unsigned int 	_level;
+	unsigned int 	_meleeDamage;
+	unsigned int 	_rangedDamage;
+	int		_damageReduction;
+};
+
+#endif
