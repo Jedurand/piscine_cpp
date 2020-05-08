4919c7f55462df3f93d6e75369dddb4c6139f23c
jukazekajou <jerome.d274@gmail.com>
2020-05-08 14:21:54 +0200

    dqsds

--- module01/ex07/sed.cpp
+++ module01/ex07/sed.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <cstdlib>
-#include <cstdio>
 #include <fstream>
 
 static int ft_error(std::string s)
@@ -24,13 +21,24 @@ int main(int ac, char **av)
 	std::string s2 = av[3];
 
 	std::ifstream src(av[1]);
+
 	if (!src)
 		return (ft_error("Path name isn't a valid one, abort ..."));
+
 	if (s1.size() == 0 || s2.size() == 0)
+	{
+		src.close();
 		return (ft_error("S1 or S2 is empty, abort ..."));
+	}
+
 	std::ofstream dest(std::string(av[1]) + ".replace");
+
 	if (!dest)
+	{
+		src.close();
 		return (ft_error("Error while creating destination file"));
+	}
+
 	while (std::getline(src, b))
 		content.append(b + "\n");
 
