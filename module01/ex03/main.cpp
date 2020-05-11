22ef3921736d1bd6dfa3adb9d3219f621a0fbd8c
jukazekajou <jerome.d274@gmail.com>
2020-05-11 18:07:22 +0200

    merde

--- module01/ex03/main.cpp
+++ module01/ex03/main.cpp
@@ -4,16 +4,15 @@
 
 void	test_leaks()
 {
-	ZombieHorde zh2(100);
+	ZombieHorde zh2(10);
 	zh2.announce();
 }
 
 int main()
 {
-	ZombieHorde *zh = new ZombieHorde(15);
+	ZombieHorde *zh = new ZombieHorde(5);
 	zh->announce();
 	delete(zh);
-	ZombieHorde zh2(100);
+	ZombieHorde zh2(15);
 	zh2.announce();
-	test_leaks();
 }
